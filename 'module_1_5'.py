1a6809b129a3b4b95f5a07f641a51320f7d1af44
Petr-max41 <krilof25@mail.ru>
2024-09-26 21:00:07 +0500

    Add files via upload

--- 'module_1_5'.py
+++ 'module_1_5'.py
@@ -2,7 +2,7 @@ immutable_var = 5, 6,7,'кортеж'
 print(immutable_var)
 #immutable_var [0] =8
 #print(immutable_var) # кортеж нельзя изменить если он несодержит изменяемых элементов.
-mutable_list = ([5,6], 2,3,4 )
+mutable_list = ['телелефон','телевизор','компьютер']
 print(mutable_list)
-mutable_list [0][0]= 7
+mutable_list [1] = 'телелефон'
 print(mutable_list)
